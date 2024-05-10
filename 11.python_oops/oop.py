ffbd1ac128816572dd42e3f6114cd2b8c0d6fe59
= <harmostparmod@gmail.com>
2024-05-10 17:37:12 +0530

    8.python oop

---
+++ 11.python_oops/oop.py
@@ -0,0 +1,54 @@
+# # from turtle import Turtle , Screen
+# #
+# # my_screen = Screen();
+# # # turt = Turtle();
+# #
+# # # print(my_screen.canvwidth)
+# # # turt.shape('turtle');
+# # # turt.color('purple');
+# # # turt.forward(100);
+# # # turt.right(90);
+# # # turt.forward(100);
+# # # turt.right(90);
+# # # turt.forward(100);
+# # # turt.right(90);
+# #
+# # raph = Turtle();
+# # raph.color("red");
+# # raph.shape("turtle");
+# #
+# # my_screen.exitonclick();
+#
+#
+# class Robot:
+#     """ This class implements a Robot """
+#     population = 0;
+#     def __init__(self , name , year):
+#         self.name = name;
+#         self.year = year;
+#         Robot.population += 1;
+#     def setEnergy(self , energy):
+#         self.energy = energy;
+#
+# r1 = Robot("R1" , "2023");
+# r2 = Robot("R2" , "2024");
+# print(r1.__doc__);
+# print(f" robot {r1.name} , made in {r1.year}");
+# r1.setEnergy(500);
+# # print(r1.__dict__)
+# print(f"robot energy is : {r1.energy}");
+# print(f"population of robot : {Robot .population}");
+
+#
+# class Circle:
+#     def __init__(self , radius):
+#         self.radius = radius;
+#
+#     def __str__(self):
+#         return str(self.radius);
+#
+#  # first circle
+# c1 = Circle(5);
+# print(c1);
+
+
